6ee903ce6cd8409a329264013393448f7f7b2fce
Ajeet Singh Rana <ajrana70@gmail.com.com>
2022-11-29 16:45:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+function Block({ list, handleClickCheckbox }) {
+  return (
+    <>
+      <div style={{ border: "1px solid black" }}>
+        {list.block}
+        {list?.checkBoxLists?.map((checkBox, index) => (
+          <div key={index}>
+            <input
+              type="checkbox"
+              id={checkBox}
+              name={checkBox}
+              value={checkBox}
+              onChange={(e) => handleClickCheckbox(e, index)}
+            />
+            <label htmlFor={checkBox}>{checkBox}</label>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
 
 function App() {
+  let charCode = 65;
+  const [lists, setList] = useState([
+    {
+      block: `Block ${String.fromCharCode(charCode++)}`,
+      checkBoxLists: ["Apple", "Mango", "Banana"],
+    },
+    {
+      block: `Block ${String.fromCharCode(charCode++)}`,
+      checkBoxLists: [],
+    },
+  ]);
+
+  const [checkedList, setCheckedList] = useState([]);
+  const handleClickCheckbox = (e) => {
+    if (e.target.checked) {
+      setCheckedList([...checkedList, e.target.value]);
+    } else {
+      checkedList.splice(checkedList.indexOf(e.target.value), 1);
+      setCheckedList([...checkedList]);
+    }
+  };
+
+  const handleFrontButton = () => {
+    // console.log("lists",checkedList);
+    lists.forEach((list, index) => {
+      // console.log("list=>",list)
+      let moveIndex = -1;
+      let moveItem = "";
+      debugger
+      list.checkBoxLists.forEach((checkBoxItem,i) => {
+        // console.log(list,checkBoxItem);
+        const ans = checkedList.includes(checkBoxItem);
+        // console.log(checkedList,checkBoxItem);
+        // console.log(ans);
+        console.count()
+        if (ans) {
+          moveIndex = i;
+          moveItem = checkBoxItem;
+          console.log(moveIndex, moveItem);
+          // console.log(moveIndex)
+          list.checkBoxLists.splice(moveIndex,1)
+          // console.log(lists);
+        }
+      });
+      console.log(list);
+
+      // if(moveIndex !== -1){
+      //   lists[++moveIndex] = moveItem
+      // }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {lists.map((list, index) => (
+        <div key={index}>
+          <Block
+            list={list}
+            handleClickCheckbox={handleClickCheckbox}
+          />
+        </div>
+      ))}
+      <button type="button">Back</button>
+      <button type="button" onClick={handleFrontButton}>
+        Front
+      </button>
+    </>
   );
 }
 
